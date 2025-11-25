918e11a521457dcfd4a8edfb3c00bbc6945cedb8
lovaraj02 <rajubandaru2255@gmail.com>
2025-11-25 10:48:25 +0530

    add loader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,39 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
 import "./App.css";
-import Navbar from "./Navbar"
-import About from "./pages/About"
-import Admin from "./pages/Admin"
-import Complaint from "./pages/Complaint"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import StayAware from "./pages/StayAware"
+
+import Navbar from "./Navbar";
+import About from "./pages/About";
+import Admin from "./pages/Admin";
+import Complaint from "./pages/Complaint";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import StayAware from "./pages/StayAware";
+
+import Loader from "./Loader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    wakeUpServer();
+  }, []);
+
+  const wakeUpServer = async () => {
+    try {
+      await fetch("https://water-quality-monitoring-da7r.onrender.com");
+      setLoading(false);
+    } catch {
+      // retry if backend is still sleeping
+      setTimeout(wakeUpServer, 2000);
+    }
+  };
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <>
       <Navbar />
@@ -21,11 +45,11 @@ function App() {
           <Route path="/Login" element={<Login />} />
           <Route path="/Admin" element={<Admin />} />
           <Route path="/about" element={<About />} />
-          <Route path="/Register" element={<Register/>}/> 
+          <Route path="/Register" element={<Register />} />
         </Routes>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/Loader.js
@@ -0,0 +1,8 @@
+export default function Loader() {
+  return (
+    <div className="loader-screen">
+      <img src="/loading.jpg" alt="Loading..." className="loader-img" />
+      <p>Loading, please wait...</p>
+    </div>
+  );
+}
