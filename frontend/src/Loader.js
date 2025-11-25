804bd2120ea2f84ad3589a3d2bee6ef1f60aaca2
lovaraj02 <rajubandaru2255@gmail.com>
2025-11-25 10:55:11 +0530

    loader working

--- frontend/src/Loader.js
+++ frontend/src/Loader.js
@@ -1,7 +1,7 @@
 export default function Loader() {
   return (
     <div className="loader-screen">
-      <img src="/loading.jpg" alt="Loading..." className="loader-img" />
+      <img src="/loader.png" alt="Loading..." className="loader-img" />
       <p>Loading, please wait...</p>
     </div>
   );
