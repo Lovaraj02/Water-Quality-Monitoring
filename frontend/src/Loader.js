640dfee08443d6e83dce9ce025b83a4c8d285409
lovaraj02 <rajubandaru2255@gmail.com>
2025-11-25 19:10:20 +0530

    mul loader

--- frontend/src/Loader.js
+++ frontend/src/Loader.js
@@ -1,7 +1,22 @@
 export default function Loader() {
+  const images = [
+    "/loader.png",
+    "/l2.png",
+    "/l3.png",
+    "/l4.png"
+  ];
+
   return (
     <div className="loader-screen">
-      <img src="/loader.png" alt="Loading..." className="loader-img" />
+      {images.map((src, i) => (
+        <img
+          key={i}
+          src={src}
+          alt={`Loading ${i}`}
+          className="loader-img"
+        />
+      ))}
+
       <p>Loading, please wait...</p>
     </div>
   );
