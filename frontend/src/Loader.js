6260dd33452e6d440a50d6f4751d6e207cdf1952
lovaraj02 <rajubandaru2255@gmail.com>
2025-12-01 09:08:51 +0530

    add loader

--- frontend/src/Loader.js
+++ frontend/src/Loader.js
@@ -1,21 +1,17 @@
-export default function Loader() {
-  const images = [
-    "/loader.png",
-    "/l2.png",
-    "/l3.png",
-    "/l4.png"
-  ];
+import { TailSpin } from "react-loader-spinner";
 
+export default function Loader() {
   return (
     <div className="loader-screen">
-      {images.map((src, i) => (
-        <img
-          key={i}
-          src={src}
-          alt={`Loading ${i}`}
-          className="loader-img"
-        />
-      ))}
+      <TailSpin
+        height={60}
+        width={60}
+        color="#000"
+        secondaryColor="#ccc"
+        strokeWidth={4}
+        visible={true}
+        ariaLabel="oval-loading"
+      />
 
       <p>Loading, please wait...</p>
     </div>
